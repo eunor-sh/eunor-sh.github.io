0293d4184b447bed0a142f33cf114ef30d18045f
eunor-sh <dmsgh122@gmail.com>
2026-01-05 01:33:18 +0900

    update

---
+++ sw.js
@@ -0,0 +1,99 @@
+const cacheName = self.location.pathname
+const pages = [
+
+  "/docs/example/",
+  "/docs/example/table-of-contents/with-toc/",
+  "/docs/example/table-of-contents/without-toc/",
+  "/posts/creating-a-new-theme/",
+  "/posts/migrate-from-jekyll/",
+  "/docs/example/table-of-contents/",
+  "/docs/example/collapsed/",
+  "/",
+  "/posts/",
+  "/posts/goisforlovers/",
+  "/categories/",
+  "/categories/Development/",
+  "/tags/development/",
+  "/posts/hugoisforlovers/",
+  "/tags/go/",
+  "/categories/golang/",
+  "/tags/golang/",
+  "/tags/hugo/",
+  "/tags/",
+  "/tags/templates/",
+  "/tags/themes/",
+  "/docs/example/collapsed/3rd-level/4th-level/",
+  "/docs/example/collapsed/3rd-level/",
+  "/docs/example/hidden/",
+  "/docs/shortcodes/",
+  "/docs/shortcodes/buttons/",
+  "/docs/shortcodes/columns/",
+  "/docs/shortcodes/details/",
+  "/docs/shortcodes/experimental/",
+  "/docs/shortcodes/experimental/asciinema/",
+  "/docs/shortcodes/experimental/badges/",
+  "/docs/shortcodes/experimental/cards/",
+  "/docs/shortcodes/experimental/images/",
+  "/docs/shortcodes/hints/",
+  "/docs/shortcodes/mermaid/",
+  "/docs/shortcodes/section/",
+  "/docs/shortcodes/section/first-page/",
+  "/docs/shortcodes/section/second-page/",
+  "/docs/shortcodes/steps/",
+  "/docs/shortcodes/tabs/",
+  "/docs/",
+  "/categories/Development/golang/",
+  "/docs/shortcodes/katex/",
+  "/showcases/",
+  "/book.min.95a684deb747f69ad4ffa2d052f0b0d03deefa43cbbfc0762b1b78ae74b27805.css",
+  "/en.search-data.min.2207505b95ec3b937695ae4ec8640e2a01b3f595b258625b6f309f46f8d9a3bf.json",
+  "/en.search.min.1aa855efc1864484e56bdb804444331b609894e93ca3ee9c6faff56e38de107a.js",
+  
+];
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+
+  caches.open(cacheName).then((cache) => {
+    return cache.addAll(pages);
+  });
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") {
+    return;
+  }
+
+  /**
+   * @param {Response} response
+   * @returns {Promise<Response>}
+   */
+  function saveToCache(response) {
+    if (cacheable(response)) {
+      return caches
+        .open(cacheName)
+        .then((cache) => cache.put(request, response.clone()))
+        .then(() => response);
+    } else {
+      return response;
+    }
+  }
+
+  /**
+   * @param {Error} error
+   */
+  function serveFromCache(error) {
+    return caches.open(cacheName).then((cache) => cache.match(request.url));
+  }
+
+  /**
+   * @param {Response} response
+   * @returns {Boolean}
+   */
+  function cacheable(response) {
+    return response.type === "basic" && response.ok && !response.headers.has("Content-Disposition")
+  }
+
+  event.respondWith(fetch(request).then(saveToCache).catch(serveFromCache));
+});
